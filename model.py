0dfba61439186438e0c7f6f43c15c7a695c0d4eb
Jeewon Lee <jeewonlee@Jeewons-MacBook-Pro.local>
2017-05-07 21:14:17 -0700

    update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.cross_validation import KFold
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.grid_search import GridSearchCV
 
@@ -44,6 +44,19 @@ def cross_val(estimator, train_x, train_y, num_folds=10):
     print '%s Train CV | f1: %.3f | accuracy: %.3f | precision: %.3f | recall: %.3f' % (name, mean_f1, mean_accuracy, mean_preicision, mean_recall)
     return mean_f1, mean_accuracy, mean_precision, mean_recall
 
+def quality(X_test, y_test, estimator):
+    # Test data accuracy score
+    print "Test score:", estimator.score(X_test, y_test)
+
+    # confusion matrix
+    y_predict = rf.predict(X_test)
+    print "Test Confusion matrix:\n"
+    print confusion_matrix(y_test, y_predict)
+
+    # Precision/Recall
+    print "Test Precision:", precision_score(y_test, y_predict)
+    print "Test Recall:", recall_score(y_test, y_predict)
+
 def run_models(train_x, train_y, cv_num=10):
     rf_grid = {'max_depth': [3, 10,20],
            'max_features': [1, 3, 10],
@@ -68,7 +81,7 @@ def run_models(train_x, train_y, cv_num=10):
                'random_state': [1]}
 
     rf_grid_search = grid_search(RandomForestRegressor(), rf_grid, train_x, train_y, num_folds=cv_num)
-    gd_grid_search = grid_search(GradientBoostClassifier(), gd_grid, train_x, train_y,num_folds=cv_num)
+    gd_grid_search = grid_search(GradientBoostingClassifier(), gd_grid, train_x, train_y,num_folds=cv_num)
     ada_grid_search = grid_search(AdaBoostClassifier, ada_grid, train_x, train_y,num_folds=cv_num)
 
     #Best models
@@ -81,6 +94,7 @@ def run_models(train_x, train_y, cv_num=10):
     cross_val(ada_best, train_x, train_y, num_folds=cv_num)
 
     return rf_best, gd_best, ada_best
+
 
 
 def plot_feature_importances(model):
@@ -102,36 +116,64 @@ def plot_feature_importances(model):
     plt.yticks(x_ind, colnames[x_ind])
     return top10_nx
 
+def feature_importance(X_test, y_test, estimator, df):
+    #Use sklearn's model to get the feature importances
+    feature_importances = np.argsort(estimator.feature_importances_)
+    print "top five:", list(df.columns[feature_importances[-1:-6:-1]])
+
+    n = 10 # top 10 features
+
+    importances = estimator.feature_importances_[:n]
+    std = np.std([tree.feature_importances_ for tree in estimator.estimators_],
+                 axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    print("Feature ranking:")
+
+    for f in range(n):
+        print("%d. %s (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title(estimator.__class__.__name__.+" Feature Importances")
+    plt.bar(range(10), importances[indices], yerr=std[indices], color="r", align="center")
+    plt.xticks(range(10), indices)
+    plt.xlim([-1, 10])
+    plt.show()
+
 def main():
     df = create_dataset()
     X, y = feature_target_split(df)
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    rf_best, gd_best, ada_best = run_models(X_train, y_train, cv_num=10):
+    rf_best, gd_best, ada_best = run_models(X_train, y_train, cv_num=10)
 
-# def grid_search
-# def kfold_model(model,num_folds = 5, x,y):
-#     kfold = KFold(len(y), n_folds = num_folds)
-#
-#     accuracies = []
-#     precisions = []
-#     recalls = []
-#
-#     for train_index, test_index in kfold:
-#         model.fit(x[train_index], y[train_index])
-#         y_predict = model.predict(x[test_index])
-#         y_true = y[test_index]
-#         accuracies.append(accuracy_score(y_true, y_predict))
-#         precisions.append(precision_score(y_true, y_predict))
-#         recalls.append(recall_score(y_true, y_predict))
-#
-#     print "accuracye:", np.average(accuracies)
-#     print "precision:", np.average(precisions)
-#     print "recall:", np.average(recalls)
+    print "\n"+rf_best.__class__.__name__
+    quality(X_test, y_test, rf_best)
+    feature_importance(X_test, y_test, gd_best, df)
 
+    print "\n"+gd_best.__class__.__name__
+    quality(X_test, y_test, gd_best)
+    feature_importance(X_test, y_test, rf_best, df)
+
+    print "\n"+ada_best.__class__.__name__
+    quality(X_test, y_test, ada_best)
+    feature_importance(X_test, y_test, ada_best, df)
 
 if __name__ == '__main__':
     df = create_dataset()
-    x, y = feature_target_split(df)
+    X, y = feature_target_split(df)
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    rf = RandomForestClassifier()
-    kfold_rf(model,num_folds = 5, X_train,y_train)
+    rf_best, gd_best, ada_best = run_models(X_train, y_train, cv_num=10)
+
+    print "\n"+rf_best.__class__.__name__
+    quality(X_test, y_test, rf_best)
+    feature_importance(X_test, y_test, gd_best, df)
+
+    print "\n"+gd_best.__class__.__name__
+    quality(X_test, y_test, gd_best)
+    feature_importance(X_test, y_test, rf_best, df)
+
+    print "\n"+ada_best.__class__.__name__
+    quality(X_test, y_test, ada_best)
+    feature_importance(X_test, y_test, ada_best, df)
